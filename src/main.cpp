c130b87eeae31b9eeef823db7cd184de5ee67206
gregorthebigmac <gregorthebigmac@gmail.com>
2019-11-07 10:31:53 -0600

    I've decided I'm going to stop here and start a new branch which will just do the wifi thing, and come back to the modular version later.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ using std::vector;
 using std::string;
 
 int main(int argc, char *argv[]) {
+	vector<vector<char> > c_args;
 	initscr();
 	if (argc < 2) {
 		printw("USAGE: checkit [PROGRAM_NAME] [PROGRAM_ARGS]");
@@ -18,7 +19,20 @@ int main(int argc, char *argv[]) {
 		endwin();
 		return 1;
 	}
+	vector<string> s_args;
+	for (int i = 1; i < argc; i++) {
+		string temp = argv[i];
+		s_args.push_back(temp);
+	}
 	
+	for (int x = 0; x < s_args.size(); x++) {
+		int arg_size = s_args[x].size();
+		char temp_c_arg [arg_size];
+		for (int y = 0; y < s_args[x].size(); y++) {
+			temp_c_arg[y] = s_args[x].at(y);
+		}
+		c_args.push_back(temp_c_arg);
+	}
 	
 	printw("argc = [");
 	printw("%i", argc);
